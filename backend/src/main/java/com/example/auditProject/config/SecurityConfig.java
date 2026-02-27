b3032f769aa8ba5bd4c1cb5efd328360d4bbdcd5
Rajyavardhan Mallampati <mallampatirv@gmail.com>
2026-02-27 14:31:41 +0530

    changed the config file

--- backend/src/main/java/com/example/auditProject/config/SecurityConfig.java
+++ backend/src/main/java/com/example/auditProject/config/SecurityConfig.java
@@ -1,3 +1,15 @@
+package com.example.auditProject.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
 @Configuration
 public class SecurityConfig {
 
